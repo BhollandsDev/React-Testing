aa8c7540f962ad970951c0499dfd3fdda2a7b8fb
BhollandsDev <bhollands2006@gmail.com>
2024-03-18 15:32:41 -0600

    Initialization

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+
+function Footer(){
+
+
+  return (
+    <footer>
+      <hr></hr>
+      <p>&copy; {new Date().getFullYear()} Your Website Name</p>
+
+    </footer>
+  )
+}
+
+export default Footer
