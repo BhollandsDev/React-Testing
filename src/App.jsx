df8bfd9882c7f6d767fe2bd9c00d33810035f278
BhollandsDev <bhollands2006@gmail.com>
2024-03-18 18:23:39 -0600

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import Button from "./Button"
+import Student from "./Students"
 function App() {
   
 return(
   <>
-  <Button/>
+    <Student name="student 1" age={50} isStudent={true}/>
+    <Student name="student 2" age={42} isStudent={false}/>
+    <Student name="student 3" age={50} isStudent={false}/>
+    <Student name="student 4" age={27} isStudent={true}/>
   </>
 )
    
